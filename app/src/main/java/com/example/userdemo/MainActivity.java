5bb0ca9c3808c8fdf1218f0a0330dd5fac8c3889
linvisf <linvisf@163.com>
2020-04-19 11:37:41 +0800

    初步界面搭建1.0

--- app/src/main/java/com/example/userdemo/MainActivity.java
+++ app/src/main/java/com/example/userdemo/MainActivity.java
@@ -1,16 +1,25 @@
 package com.example.userdemo;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.FrameLayout;
 import android.widget.TextView;
 
 import com.example.userdemo.bean.BannerBean;
 import com.example.userdemo.bean.GankBean;
 import com.example.userdemo.bean.HttpResultBean;
 import com.example.userdemo.http.BaseAPI;
+import com.example.userdemo.ui.fragment.CategoryFragment;
+import com.example.userdemo.ui.fragment.MainFragment;
+import com.example.userdemo.ui.fragment.MeiZhiFragment;
+import com.example.userdemo.ui.fragment.MyFragment;
 
 import java.util.List;
 
@@ -18,16 +27,25 @@ import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     private final static String TAG = "MainActivity";
 
-//    private TextView textView;
+    private Button btn_hot;
+    private Button btn_category;
+    private Button btn_mei;
+    private Button btn_my;
+    private FrameLayout layout_constant;
+
+    private FragmentManager fragmentManager;
+    private String curFragmentTag;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
+        initData();
 //        textView = findViewById(R.id.textView);
 //        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
 //            @Override
@@ -37,6 +55,53 @@ public class MainActivity extends AppCompatActivity {
 //            }
 //        });
 
+    }
+
+    private void initView() {
+        layout_constant = findViewById(R.id.layout_constant);
+        btn_hot = findViewById(R.id.btn_hot);
+        btn_category = findViewById(R.id.btn_category);
+        btn_mei = findViewById(R.id.btn_mei);
+        btn_my = findViewById(R.id.btn_my);
+        btn_hot.setOnClickListener(this);
+        btn_category.setOnClickListener(this);
+        btn_mei.setOnClickListener(this);
+        btn_my.setOnClickListener(this);
+    }
+
+    private void initData() {
+        fragmentManager = getSupportFragmentManager();
+        switchFragment("hot");
+    }
+
+    private void switchFragment(String name){
+        if (curFragmentTag != null && curFragmentTag.equals(name))
+            return;
+        FragmentTransaction ft = fragmentManager.beginTransaction();
+        ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_CLOSE);
+        Fragment currFragment = fragmentManager.findFragmentByTag(curFragmentTag);
+        if (currFragment != null)
+            ft.hide(currFragment);
+
+        Fragment foundFragment = fragmentManager.findFragmentByTag(name);
+        if (foundFragment == null) {
+            if (name.equals("hot")){
+                foundFragment = new MainFragment();
+            } else if (name.equals("category")) {
+                foundFragment = new CategoryFragment();
+            } else if (name.equals("meizhi")) {
+                foundFragment = new MeiZhiFragment();
+            } else if (name.equals("my")) {
+                foundFragment = new MyFragment();
+            }
+        }
+        if (foundFragment.isAdded()) {
+            ft.show(foundFragment);
+        } else {
+            ft.add(R.id.layout_constant, foundFragment, name);
+        }
+        ft.commit();
+        curFragmentTag = name;
     }
 
     private void getBanner() {
@@ -76,5 +141,23 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_hot:
+                switchFragment("hot");
+                break;
+            case R.id.btn_category:
+                switchFragment("category");
+                break;
+            case R.id.btn_mei:
+                switchFragment("meizhi");
+                break;
+            case R.id.btn_my:
+                switchFragment("my");
+                break;
+        }
     }
 }
